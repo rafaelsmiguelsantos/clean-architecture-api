eeacb237ac6ec2ba3de8d935e6e3cf8a9f0fb199
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-04-04 20:25:37 -0300

    chore: add jest config init

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: { '.+\\.ts$': 'ts-jest' },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'v8'
+}
