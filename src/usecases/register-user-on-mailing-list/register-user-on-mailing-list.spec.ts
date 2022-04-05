b9c2415a9d5e037a5f46b6621ac191d4900ae6e0
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-04-04 22:16:18 -0300

    test: add test to use case register user on mailing list

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,4 @@
-import { UserData } from "../user-data"
+import { UserData } from '../user-data'
 
 describe('Register user on mailing list use case', () => {
   test('Should add user with complete data to mailing list', async () => {
