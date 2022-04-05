addc030174d858d8c9a838110c8a8d7096d649c2
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-04-04 22:13:23 -0300

    test: add test to use case register user on mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from "../user-data"
+
+describe('Register user on mailing list use case', () => {
+  test('Should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecases: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecases.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
